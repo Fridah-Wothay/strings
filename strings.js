43135d40d27fa1965a73e5fefe18166d67e94f64
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-07 09:29:27 +0300

    add file

---
+++ strings.js
@@ -0,0 +1,42 @@
+let a = "extravaganza"
+console.log(a[8]+a[9]+a[10]+a[11]);
+
+let b = "The quick brown fox jumped over the lazy dog"
+let c = "the"
+let d = "brown"
+console.log((b.match(/the/)).length);
+console.log((b.match(/brown/)).length);
+
+let e = "the pupils are reading in the library"
+let f ="are"
+let g = "the child was sitting on the table before it fell off"
+let h ="sitting"
+console.log(e.includes(f));
+console.log(g.includes(h));
+
+let originString = "The quick fox jumped over the lazy dog";
+let StringToAdd = "eat"
+let indexPosition = 4;
+newString = originString.slice(0,indexPosition)+ StringToAdd + originString.slice(indexPosition);
+console.log(newString);
+
+let i = "wonderful"
+let uppercase = i.toUpperCase();
+console.log(uppercase);
+
+let j = "amazing"
+let lowercase = j.toLowerCase();
+console.log(lowercase);
+
+let k = "underneath"
+let Lowercase = k .toLowerCase();
+console.log(Lowercase);
+
+const title = "A wonderful world"
+let titleCase = "";
+title.split(" ").forEach(word => {
+    const capitalizeWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    titleCase += capitalizeWord + " ";
+
+});
+    console.log(titleCase);
