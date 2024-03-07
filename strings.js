a32cd2febdf3ff9c9e07324a85efea5107cde693
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-07 11:06:50 +0300

    add changes

--- strings.js
+++ strings.js
@@ -1,12 +1,15 @@
+//no.1
 let a = "extravaganza"
 console.log(a[8]+a[9]+a[10]+a[11]);
 
+//no.2
 let b = "The quick brown fox jumped over the lazy dog"
 let c = "the"
 let d = "brown"
 console.log((b.match(/the/)).length);
 console.log((b.match(/brown/)).length);
 
+//no.4
 let e = "the pupils are reading in the library"
 let f ="are"
 let g = "the child was sitting on the table before it fell off"
@@ -14,12 +17,15 @@ let h ="sitting"
 console.log(e.includes(f));
 console.log(g.includes(h));
 
+//no.3
+
 let originString = "The quick fox jumped over the lazy dog";
 let StringToAdd = "eat"
 let indexPosition = 4;
 newString = originString.slice(0,indexPosition)+ StringToAdd + originString.slice(indexPosition);
 console.log(newString);
 
+//no.5
 let i = "wonderful"
 let uppercase = i.toUpperCase();
 console.log(uppercase);
